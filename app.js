ba87af5a5c04829aaa67f4ecc1acb5fb37cf1cf9
Raymond <raymondjohnson@pursuit.org>
2024-11-05 22:11:30 -0500

    server initialized

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const db = require('./db/dbConfig.js')
+// const userController = require('./controllers/userController')
+
+const cors = require('cors')
+app.use(cors())
+
+// app.use('/users', userController )
+
+app.get('/', (req, res) => {
+  res.send (`job-search-app-be`)
+})
+app.get('*', (req, res) => {
+  res.send (`This file is not available!`)
+})
+
+module.exports = app;
+
